022e65ec0a03cbd25e8df5f7dc75c865aa6bc2c9
friesenkatharina <friesen.katharina2022@gmail.com>
2023-10-20 08:42:06 +0200

    improved html, css, js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import'../styles/style.css';
-import'../styles/main.css';
-import'../styles/header.css';
-import'../styles/hero.css';
-import'../styles/about.css';
-import'../styles/footer.css';
-import'../styles/mobile-nav.css';
-import mobileNav from './mobile/mobile-nav';
-import darkMode from './mobile/dark-mode';
+// Button-Element mit der ID "theme-toggle"
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
 
-mobileNav();
+//  Event-Listener  um auf Klickereignisse zu reagieren
+themeToggle.addEventListener("click", function () {
+  // Finde das <i>-Element innerhalb des Buttons
+  const lightbulbIcon = themeToggle.querySelector("i.fa-lightbulb");
+
+  // Überprüfe, ob das <i>-Element die Klasse "active" hat
+  if (lightbulbIcon.classList.contains("active")) {
+    // Schalte die Glühbirne aus, indem die Klasse "active" entfernt wird
+    lightbulbIcon.classList.remove("active");
+    // Ändere die Hintergrundfarbe der Seite zurück
+    body.style.backgroundColor = "var(--clr-indigo950)";
+  } else {
+    // Schalte die Glühbirne ein, indem die Klasse "active" hinzugefügt wird
+    lightbulbIcon.classList.add("active");
+    // Ändere die Hintergrundfarbe der Seite auf eine neue Farbe
+    body.style.backgroundColor = "var(--deine-neue-hintergrundfarbe)";
+  }
+});
--- src/mobile/dark-mode.js
+++
@@ -1,9 +0,0 @@
-// const darkMode = () => {
-// const themeToggleBtns = document.querySelectorAll('#theme-toggle');
-// themeToggleBtns.forEach( btn =>{
-//     btn.addEventListener('click',() => {
-//         document.body.classList.toggle('light-mode');
-//     });
-// }); 
-// };
-// export default darkMode;
