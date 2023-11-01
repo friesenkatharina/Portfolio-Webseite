6fcdbc680433d821a60a9848cf99fcc9519ca896
friesenkatharina <friesen.katharina2022@gmail.com>
2023-11-01 17:11:23 +0100

    style improvement, add calculator

--- src/script.js
+++ src/script.js
@@ -2,3 +2,25 @@ function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+// calculator
+function appendOperation(operation) {
+  document.getElementById("resultArea").innerHTML += operation;
+}
+
+function calculateResult() {
+  let container = document.getElementById("resultArea");
+  let result = eval(container.innerHTML);
+  container.innerHTML = result;
+}
+
+// durch die .slice()-Methode wird die letzte Zahl wenn ich auf DEL klicke gelöscht
+function deleteLast() {
+  let container = document.getElementById("resultArea");
+  if (container.innerHTML.endsWith("")) {
+    container.innerHTML = container.innerHTML.slice(0, -3);
+  } else {
+    container.innerHTML = container.innerHTML.slice(0, -1);
+  }
+  container.innerHTML = container.innerHTML.slice(0, -1);
+}
