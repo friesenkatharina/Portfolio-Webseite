89cf065dfa7a0ff27b1b12d7704a36b81336ef3a
friesenkatharina <friesen.katharina2022@gmail.com>
2023-11-06 20:33:57 +0100

    fixed responsive for small screens

--- RockPaperScissors-JS-Game/app.js
+++ RockPaperScissors-JS-Game/app.js
@@ -98,3 +98,58 @@ function main() {
   scissors_div.addEventListener("click", () => game("s"));
 }
 main();
+
+// IONIC FRAMEWORK FUnction => does not work
+
+// async function presentAlert() {
+//   const alert = document.createElement("ion-alert");
+//   alert.header = "Alert";
+//   alert.subHeader = "Important message";
+//   alert.message = "This is an alert!";
+//   alert.buttons = ["OK"];
+
+//   document.body.appendChild(alert);
+//   await alert.present();
+// }
+// import { alertController } from "@ionic/core";
+
+// async function presentAlert() {
+//   const alert = await alertController.create({
+//     header: "Alert",
+//     subHeader: "Important message",
+//     message: "This is an alert!",
+//     buttons: ["OK"],
+//   });
+
+//   return alert.present();
+// }
+// // Das "window" Objekt wird hier verwendet, um die Funktion "presentAlert"
+// // global zugänglich zu machen, damit sie im "onclick" Ereignis aufgerufen werden kann.
+// window.presentAlert = presentAlert;
+
+// ALERT
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  modal.style.alignItems = "center";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "block";
+  }
+};
