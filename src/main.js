1b2d1060199232e26c40f74b20a39bc1cb1f36f0
friesenkatharina <friesen.katharina2022@gmail.com>
2024-03-18 15:01:13 +0100

    remove slider

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// // slider IMG  need to be updated
-
-// const slider = document.querySelector(".slider input");
-// const dragLine = document.querySelector(".slider input");
-// slider.oninput = () => {
-//   let sliderVal = slider.value;
-//   dragLine.style.left = sliderVal + "%";
-// };
