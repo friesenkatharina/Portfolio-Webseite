dea8e13a11027892bf7d739a9eef54ad1270c0e2
friesenkatharina <friesen.katharina2022@gmail.com>
2024-07-31 16:20:43 +0200

    fixed buck:[update email.js to use config file for serviceId, templateId and publicKey]

--- src/email.js
+++ src/email.js
@@ -1,24 +1,24 @@
+import config from '../config.js';
+
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contact-form");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+    const form = document.getElementById("contact-form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const serviceId = "service_i2pt7ql";
-    const templateId = "template_lhcwv5e";
-    const publicKey = "Hexgh2WaL894SwgtW";
+        const { serviceId, templateId, publicKey } = config;
 
-    emailjs.init(publicKey);
+        emailjs.init(publicKey);
 
-    emailjs.sendForm(serviceId, templateId, this).then(
-      function () {
-        console.log("Email successfully sent!");
-        alert("Message sent!");
-        form.reset(); // Reset form after submission
-      },
-      function (error) {
-        console.error("Failed to send email:", error);
-        alert("Something went wrong, please try again later.");
-      }
-    );
-  });
+        emailjs.sendForm(serviceId, templateId, this).then(
+            function () {
+                console.log("Email successfully sent!");
+                alert("Message sent!");
+                form.reset(); // Reset form after submission
+            },
+            function (error) {
+                console.error("Failed to send email:", error);
+                alert("Something went wrong, please try again later.");
+            }
+        );
+    });
 });
